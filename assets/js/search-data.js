9cbaedcc6e2bd87f4e58cac79a3090feb301dac2
NeoNeuron <NeoNeuron@users.noreply.github.com>
2026-08-29 10:15:07 +0000

    Deploying to gh-pages from @ NeoNeuron/neoneuron.github.io@7770826556b11097685902121f72553883fb167f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,7 +1,7 @@
 
 const currentUrl = window.location.href;
-const siteUrl = "https://neoneuron.github.io"; 
-let updatedUrl = currentUrl.replace("https://neoneuron.github.io", "");
+const siteUrl = "https://kchen513.top"; 
+let updatedUrl = currentUrl.replace("https://kchen513.top", "");
 if (currentUrl.length == updatedUrl.length && currentUrl.startsWith("http://127.0.0.1")) {
   const otherSiteUrl = siteUrl.replace("localhost", "127.0.0.1");
   updatedUrl = currentUrl.replace(otherSiteUrl + "", "");
